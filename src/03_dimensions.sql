ca9e6759d13b7852064f4bae9b10810aa4cd2825
cadupee1464 <cadupee@icloud.com>
2026-03-25 13:51:23 -0700

    Update 03_dimensions.sql to use true boolean

--- src/03_dimensions.sql
+++ src/03_dimensions.sql
@@ -1,35 +1,35 @@
 CREATE TABLE student AS
 SELECT 
 DISTINCT StudentIdentifier,
-CASE WHEN TRIM(UPPER(MigrantStatus)) = 'YES' THEN 1
-WHEN TRIM(UPPER(MigrantStatus)) = 'NO' THEN 0
+CASE WHEN TRIM(UPPER(MigrantStatus)) = 'YES' THEN TRUE
+WHEN TRIM(UPPER(MigrantStatus)) = 'NO' THEN FALSE
 END AS IsMigrant,
 LanguageCode,
 LanguageAltCode,
 EnglishLanguageAcquisitionStatus,
-CASE WHEN TRIM(UPPER(HispanicOrLatinoEthnicity)) = 'YES' THEN 1
-WHEN TRIM(UPPER(HispanicOrLatinoEthnicity)) = 'NA' THEN 0
+CASE WHEN TRIM(UPPER(HispanicOrLatinoEthnicity)) = 'YES' THEN TRUE
+WHEN TRIM(UPPER(HispanicOrLatinoEthnicity)) = 'NA' THEN FALSE
 END AS IsHispanicOrLatino,
-CASE WHEN TRIM(UPPER(AmericanIndianOrAlaskaNative)) = 'YES' THEN 1
-WHEN TRIM(UPPER(AmericanIndianOrAlaskaNative)) = 'NA' THEN 0
+CASE WHEN TRIM(UPPER(AmericanIndianOrAlaskaNative)) = 'YES' THEN TRUE
+WHEN TRIM(UPPER(AmericanIndianOrAlaskaNative)) = 'NA' THEN FALSE
 END AS IsAmericanIndianOrAlaskaNative,
-CASE WHEN TRIM(UPPER(Asian)) = 'YES' THEN 1
-WHEN TRIM(UPPER(Asian)) = 'NA' THEN 0
+CASE WHEN TRIM(UPPER(Asian)) = 'YES' THEN TRUE
+WHEN TRIM(UPPER(Asian)) = 'NA' THEN FALSE
 END AS IsAsian,
-CASE WHEN TRIM(UPPER(BlackOrAfricanAmerican)) = 'YES' THEN 1
-WHEN TRIM(UPPER(BlackOrAfricanAmerican)) = 'NA' THEN 0
+CASE WHEN TRIM(UPPER(BlackOrAfricanAmerican)) = 'YES' THEN TRUE
+WHEN TRIM(UPPER(BlackOrAfricanAmerican)) = 'NA' THEN FALSE
 END AS IsBlackOrAfricanAmerican,
-CASE WHEN TRIM(UPPER(White)) = 'YES' THEN 1
-WHEN TRIM(UPPER(White)) = 'NA' THEN 0
+CASE WHEN TRIM(UPPER(White)) = 'YES' THEN TRUE
+WHEN TRIM(UPPER(White)) = 'NA' THEN FALSE
 END AS IsWhite,
-CASE WHEN TRIM(UPPER(NativeHawaiianOrOtherPacificIslander)) = 'YES' THEN 1
-WHEN TRIM(UPPER(NativeHawaiianOrOtherPacificIslander)) = 'NA' THEN 0
+CASE WHEN TRIM(UPPER(NativeHawaiianOrOtherPacificIslander)) = 'YES' THEN TRUE
+WHEN TRIM(UPPER(NativeHawaiianOrOtherPacificIslander)) = 'NA' THEN FALSE
 END AS IsNativeHawaiianOrOtherPacificIslander,
-CASE WHEN TRIM(UPPER(TwoOrMoreRaces)) = 'YES' THEN 1
-WHEN TRIM(UPPER(TwoOrMoreRaces)) = 'NA' THEN 0
+CASE WHEN TRIM(UPPER(TwoOrMoreRaces)) = 'YES' THEN TRUE
+WHEN TRIM(UPPER(TwoOrMoreRaces)) = 'NA' THEN FALSE
 END AS IsTwoOrMoreRaces,
-CASE WHEN TRIM(UPPER(Filipino)) = 'YES' THEN 1
-WHEN TRIM(UPPER(Filipino)) = 'NA' THEN 0
+CASE WHEN TRIM(UPPER(Filipino)) = 'YES' THEN TRUE
+WHEN TRIM(UPPER(Filipino)) = 'NA' THEN FALSE
 END AS IsFilipino
 FROM stg;
 
